13713c888002462435bf7e93b5bae191dd54b0f1
lmh <minhyeok.lee1@gmail.com>
2023-07-05 09:29:22 +0900

    Hero Aniamtion lg:ml-20

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -4,13 +4,13 @@ import Animation from "./animation";
 export default function Hero() {
   return (
     <>
-      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 lg:ml-20">
         <Animation />
       </div>
       <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           안녕하세요!
-          <br className="hidden lg:inline-block" />
+          <br className="inline-block" />
           minhyeok.lee1의 웹사이트 입니다.
         </h1>
         <p className="mb-8 leading-relaxed">
