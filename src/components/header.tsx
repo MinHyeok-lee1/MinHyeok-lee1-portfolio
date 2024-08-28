cfc38496f22d447d5e74f4992f77437851d2e98c
lmh <minhyeok.lee1@gmail.com>
2024-08-28 23:14:11 +0900

    💄style(header): Change header contents align row

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -284,7 +284,7 @@ export default function Header() {
                           dark:text-slate-400
                           dark:hover:text-yellow-500"
                 >
-                  {/* darkmode */}
+                  {/* dark mode */}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -299,7 +299,7 @@ export default function Header() {
                       d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
                     />
                   </svg>
-                  {/* lightmode */}
+                  {/* light mode */}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -314,7 +314,7 @@ export default function Header() {
                       d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
                     />
                   </svg>
-                  <span>Dark</span>
+                  <span className="pb-0.5">Dark</span>
                 </button>
               </div>
             </div>
