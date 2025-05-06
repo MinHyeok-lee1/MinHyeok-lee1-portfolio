e874b71b7896f1560141f4182d765ec392c4d30f
lmh <minhyeok.lee1@gmail.com>
2025-05-06 13:53:53 +0900

    🐛fix: Fix animaition.tsx

--- src/components/home/animation.tsx
+++ src/components/home/animation.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import Lottie from "react-lottie-player";
+// components/Animation.tsx
+"use client";
+
+import dynamic from "next/dynamic";
 import lottieJson from "../../../public/aniJson.json";
+
+// SSR 끄고 클라이언트에서만 로딩
+const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 export default function Animation() {
   return <Lottie loop animationData={lottieJson} play />;
