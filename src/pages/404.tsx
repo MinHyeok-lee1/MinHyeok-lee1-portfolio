472cd6fc1fa59fb69cfdaec97533d2fe63357c37
lmh <minhyeok.lee1@gmail.com>
2025-05-07 17:17:20 +0900

    💄 style: Change 404 ui

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,17 @@
+import { Link } from "nextra-theme-docs";
+
 export default function Custom404() {
-  return <h1>페이지를 찾을 수 없습니다.</h1>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-200 text-gray-800">
+      <h1 className="text-8xl font-bold mb-4">404</h1>
+      <p className="text-2xl mb-6">페이지를 찾을 수가 없어요</p>
+      <p className="text-sm mb-6">아래 버튼을 눌러 홈으로 돌아가주세요</p>
+
+      <Link href="/">
+        <button className="px-6 py-2 bg-slate-600 text-white rounded hover:bg-slate-700 transition">
+          홈으로
+        </button>
+      </Link>
+    </div>
+  );
 }
