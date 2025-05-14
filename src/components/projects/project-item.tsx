cd424a13eb755bc8c06db90110dea6afa2d49800
lmh <minhyeok.lee1@gmail.com>
2025-05-14 22:21:03 +0900

    🐛fix: Add image loading spinner
    lmh

--- src/components/projects/project-item.tsx
+++ src/components/projects/project-item.tsx
@@ -72,7 +72,31 @@ export default function ProjectItem({ data }: ProjectItemProps) {
             sizes="auto"
             quality={100}
             onError={() => setImageError(true)}
+            priority // 이미지 미리 로드
           />
+        ) : !imgSrc ? (
+          <div className="flex justify-center items-center w-full h-full">
+            <svg
+              className="animate-spin h-6 w-6 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
+              ></path>
+            </svg>
+          </div>
         ) : (
           <span className="text-sm text-gray-500">cover image</span>
         )}
