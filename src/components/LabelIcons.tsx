16dec6695f5cd8bcbe080f6c4b35c829353c0a63
lmh <minhyeok.lee1@gmail.com>
2025-07-07 15:49:33 +0900

    💄style: Fix icon is invisible when darkmode
    lmh

--- src/components/LabelIcons.tsx
+++ src/components/LabelIcons.tsx
@@ -1,31 +1,12 @@
 // components/LabelIcons.tsx
 export default function LabelIcons() {
   return (
-    <span
-      style={{
-        display: "inline-flex",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src="/vercel.svg"
-        width={18}
-        style={{
-          marginRight: 4,
-        }}
-        alt="Vercel"
-      />
-      Nextra(포트폴리오)
-      <span style={{ margin: "0 8px" }}>|</span>
-      <img
-        src="/github.svg"
-        width={24}
-        style={{
-          marginRight: 1,
-        }}
-        alt="GitHub"
-      />
-      GitHub(원본)
+    <span className="inline-flex items-center bg-transparent dark:bg-[#94A3B8] rounded px-2 py-1">
+      <img src="/vercel.svg" width={18} className="mr-1.5" alt="Vercel" />
+      <span className="text-black dark:text-black">Nextra(포트폴리오)</span>
+      <span className="mx-2 text-black">|</span>
+      <img src="/github.svg" width={24} className="mr-1" alt="GitHub" />
+      <span className="text-black dark:text-black">GitHub(원본)</span>
     </span>
   );
 }
